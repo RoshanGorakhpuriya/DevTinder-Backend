1081815b756e977318e7888458e8ccfcd4193d89
Roshan Gorakhpuriya <roshangorakhpuriya@gmail.com>
2025-02-13 23:45:24 +0100

    Feature : Chat Implementation

--- src/app.js
+++ src/app.js
@@ -8,6 +8,8 @@ const cookieParser = require("cookie-parser");
 
 const cors = require("cors");
 
+const http = require("http");
+
 app.use(cors({
     origin : "http://localhost:5173",
     credentials : true,
@@ -22,15 +24,23 @@ const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 
 const userRouter = require("./routes/user");
+const initializeSocket = require("./utils/socket");
+const chatRouter = require("./routes/chat");
+
 
 app.use("/" , authRouter);
 app.use("/" , profileRouter);
 app.use("/" , requestRouter);
 app.use("/" , userRouter);
+app.use("/" , chatRouter);
+
+const server = http.createServer(app);
+
+initializeSocket(server);
 
 connectDB().then(()=>{
     console.log("Database connection established");
-    app.listen(7777 , ()=>{
+    server.listen(7777 , ()=>{
         console.log("Server Launched Successfully");
     })
 })
